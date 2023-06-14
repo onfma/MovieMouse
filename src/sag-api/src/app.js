49af15252cf53af6f4a27780df4e2ca4f0214558
iustina0 <iustinaapostol02@gmail.com>
2023-06-14 12:56:46 +0300

    Update app.js

--- src/sag-api/src/app.js
+++ src/sag-api/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const actorsRoutes = require('./routes/actorsRoutes');
 const seriesRoutes = require('./routes/seriesRoutes');
 const moviesRoutes = require('./routes/moviesRoutes');
 
 const app = express();
+app.use(express.static('public'));
 
+app.use(cors());
 app.use('/', actorsRoutes);
 app.use('/', seriesRoutes);
 app.use('/', moviesRoutes);
